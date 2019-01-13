26e1f2d558c67eaf2f721db790c1374ea9c30cd3
F1703 <franco170391@gmail.com>
2019-01-13 11:33:31 -0300

    implementar funciones

--- app/src/main/java/com/example/franco/kotlinexample/MainActivity.kt
+++ app/src/main/java/com/example/franco/kotlinexample/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.franco.kotlinexample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextWatcher {
+
 
     private var editName: EditText ?=null
     private var editAge: EditText?=null
@@ -20,9 +23,21 @@ class MainActivity : AppCompatActivity() {
 
         editName = findViewById(R.id.editText_Name) as EditText
         editAge = findViewById(R.id.editText_Age) as EditText
-        
+
+
+        //implemento interface
+        editName!!.addTextChangedListener(this)
+
+    }
 
 
 
+    override fun afterTextChanged(s: Editable?) {
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
     }
 }
