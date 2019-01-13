1cd93a29861920c83440c4c567307d39bd4f724c
F1703 <franco170391@gmail.com>
2019-01-13 11:10:08 -0300

    firts

---
+++ app/src/main/java/com/example/franco/kotlinexample/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.franco.kotlinexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    private var editName: EditText ?=null
+    private var editAge: EditText?=null
+
+    private var name: String?=null
+    private var age: String?=null
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editName = findViewById(R.id.editText_Name)
+        
+
+
+    }
+}
